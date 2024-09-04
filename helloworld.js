af12c9638f1a3f8e3d5613e6b7aa897a61887d66
mikeyrat <meforman@yahoo.com>
2024-09-04 11:11:31 -0400

    Update HelloWorld
    All fixed now

--- helloworld.js
+++ helloworld.js
@@ -7,8 +7,8 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
-   // Send the response body as "Hello World"
-   response.end('Hello World\n')
+   // Send the response body as "Hello Ya'll from Mikeyrat Formicaman!!"
+   response.end('Hello Yall from Mikeyrat Formicaman!!\n')
 }).listen(3333)
 
 // Console will print the message

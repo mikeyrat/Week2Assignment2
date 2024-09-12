ed2a6f719d02e2ea03bde002b21e47d135830fc3
mikeyrat <meforman@yahoo.com>
2024-09-11 21:34:36 -0400

    Updated HelloWorld File
    changed the text on the web page for week 3 assignment

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello Ya'll from Mikeyrat Formicaman!!"
-   response.end('Hello Yall from Mikeyrat Formicaman!!\n')
+   response.end('Hello Yall from Mikeyrat Formicaman!! Newly Modified for Ansible assignment!\n')
 }).listen(3333)
 
 // Console will print the message

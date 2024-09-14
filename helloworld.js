354593c8664f67231bcf15645accff6fac33b198
mikeyrat <157513564+mikeyrat@users.noreply.github.com>
2024-09-14 13:32:03 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello Ya'll from Mikeyrat Formicaman!!"
-   response.end('Hello Yall from Mikeyrat Formicaman!! Newly Modified for Ansible assignment!\n')
+   response.end('Hello Yall from Mikeyrat Formicaman!! Newly Modified for Ansible assignment! Now updated with Ansible-playbook!\n')
 }).listen(3000)
 
 // Console will print the message
